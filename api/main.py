6a8b03193228d92102f66766d67c989fdd057b95
Haribhau Hud <hhud@nvidia.com>
2024-01-03 13:28:55 +0530

    Query and Path params

--- api/main.py
+++ api/main.py
@@ -13,7 +13,20 @@ def movies():
         "Movies": ['movie1', 'movie2']
     }
 
+# Path Parameter: parameters passed in path
 @app.get(path='/profile/{user}')
 def get_profile(user: str):
     return {f"This is a profile page for - {user}"}
 
+# Query Parameter: When parameter is passed in func but not in path
+# to make query params optional we can set their values to None.
+# /products?id=1&price=100
+@app.get('/products')
+def products(id: int = 1, price: str = None):
+    return {f"Product with an Id: {id} and price: {price}"}
+
+# Query + Path parameter
+# /profile/{id}/comments?commentid=123
+@app.get('/profile/{id}/comments')
+def comments(id: int, commentid: int):
+    return {f"Profile page for user Id: {id} and comment: {commentid}"}
