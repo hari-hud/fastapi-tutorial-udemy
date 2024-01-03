fcefc1d3fe61b1e0dd70fa6d9c12a23f8385d62f
Haribhau Hud <hhud@nvidia.com>
2024-01-03 15:42:21 +0530

    Request body using Pydantic

--- api/main.py
+++ api/main.py
@@ -1,32 +1,48 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get(path='/')
-def index():
-    return 'Hello, World!'
+class Product(BaseModel):
+    name: str
+    price: float
+    discount: int
+    discounted_price: float
 
-@app.get(path='/movies')
+
+@app.get(path="/")
+def index():
+    return "Hello, World!"
+
+
+@app.get(path="/movies")
 def movies():
-    return {
-        "Movies": ['movie1', 'movie2']
-    }
+    return {"Movies": ["movie1", "movie2"]}
+
 
 # Path Parameter: parameters passed in path
-@app.get(path='/profile/{user}')
+@app.get(path="/profile/{user}")
 def get_profile(user: str):
     return {f"This is a profile page for - {user}"}
+
 
 # Query Parameter: When parameter is passed in func but not in path
 # to make query params optional we can set their values to None.
 # /products?id=1&price=100
-@app.get('/products')
+@app.get("/products")
 def products(id: int = 1, price: str = None):
     return {f"Product with an Id: {id} and price: {price}"}
 
+
 # Query + Path parameter
 # /profile/{id}/comments?commentid=123
-@app.get('/profile/{id}/comments')
+@app.get("/profile/{id}/comments")
 def comments(id: int, commentid: int):
     return {f"Profile page for user Id: {id} and comment: {commentid}"}
+
+
+@app.post("/products")
+def create_product(product: Product):
+    product.discounted_price = product.price - (product.price * product.discount) / 100
+    return product
